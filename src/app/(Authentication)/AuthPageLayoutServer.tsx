9ec90fb1cd3709c3372af44305859f41e8d0d7ad
Ahmed Nashaat <anashaat95@gmail.com>
2025-02-28 18:27:58 +0200

    add the feature image upload for equipments

--- src/app/(Authentication)/AuthPageLayoutServer.tsx
+++ src/app/(Authentication)/AuthPageLayoutServer.tsx
@@ -1,22 +1,13 @@
 // Server-side component (AuthPageLayoutServer.tsx)
 import CustomLoader from "@/components/CustomLoader";
-import { GetJwtTokenPayload } from "@/services/jwtTokenService";
-import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import "server-only";
 import { IAuthPageLayout } from "./authInterfaces";
 import AuthPageLayoutClient from "./AuthPageLayoutClient"; // Client Component
 
-let trials = 0;
-
 const AuthPageLayoutServer = async ({ title, description, gridSizes, children }: IAuthPageLayout) => {
-  const token = await GetJwtTokenPayload();
-  if (token && trials < 3) {
-    redirect("/dashboard");
-    trials += 1;
-  } else {
-    trials = 0;
-  }
+  // const token = await GetJwtTokenPayload();
+  // if (token) redirect("/dashboard");
 
   return (
     <Suspense fallback={<CustomLoader />}>
